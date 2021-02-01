86796d84a7aeb0e0d05d86c9995ad98d768b6672
ThaisCampanac <thais.campanac@gmail.com>
2021-01-31 22:45:52 -0600

    First commit

--- challenge.py
+++ challenge.py
@@ -1,13 +1,25 @@
+content = []
+name_to_search = 'ORGANISM'
+version_to_search = 'VERSION'
+list_of_features = []
+
 def getFile():
-    f = open("Genome.gb")
-    for line in f:
-        line = line.strip()
-        for word in line.split():
-            if word == "Source":
-            #take line after that
+    f = open("Genome.gb", "r")
+
+    f1 = f.readlines()
+
+    for x in f1:
+        content.append(x.strip())
+        if name_to_search in x:
+            list_of_features.append(x.strip())
+        if version_to_search in x:
+            list_of_features.append(x.strip())
+    print(list_of_features)
+
 
 def main():
     print("Getting contents of the file")
+    print()
     getFile()
 
 if __name__ == "__main__":
