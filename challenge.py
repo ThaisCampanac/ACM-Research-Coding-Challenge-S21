9d4a18a4507c0ddd9c1d1d71de82fc83dcab9573
ThaisCampanac <thais.campanac@gmail.com>
2021-02-01 12:34:45 -0600

    Update

--- challenge.py
+++ challenge.py
@@ -1,23 +1,21 @@
 from Bio import SeqIO, GenBank
+from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio.Graphics import GenomeDiagram
-
-content = []
-name_to_search = 'ORGANISM'
-version_to_search = 'VERSION'
-list_of_features = []
+from reportlab.lib import colors
+from reportlab.lib.units import cm
 
 def getFile():
+    #reading the file
     record = SeqIO.read("Genome.gb", "genbank")
     print(record)
-    print(record.seq)
 
+    #making the gd_diagram for the genome
     gd_diagram = GenomeDiagram.Diagram(record.id)
-    gd_track_for_features = gd_diagram.new_track(2, name="Annotated Features")
-    gd_track_for_content = gd_diagram.new_track(1, name ="Annotated Content")
+    gd_track_for_features = gd_diagram.new_track(1, name="Annotated Features")
     gd_feature_set = gd_track_for_features.new_set()
-    gd_content_set = gd_track_for_content.new_set()
     
 
+    #adding the features to the gd_diagram
     for feature in record.features:
         if feature.type != "gene":
             continue
@@ -27,14 +25,21 @@ def getFile():
             color = colors.lightblue
         gd_feature_set.add_feature(feature, color=color, label_size=10, label_angle=0)
 
+
+    #making the circular diagram
     gd_diagram.draw(
         format="circular",
         circular=True,
-        pagesize=(20, 20),
+        pagesize=(20 * cm, 20 * cm),
         start=10,
         end = len(record),
         circle_core=.7
     )
+
+    #writing the diagram to all formats asked by the client
+    gd_diagram.write("genome_circular_diagram.png", "PNG")
+    gd_diagram.write("genome_circular_diagram.jpeg", "JPEG")
+    gd_diagram.write("genome_circular_diagram.jpg", "JPG")
 
 
 def main():
